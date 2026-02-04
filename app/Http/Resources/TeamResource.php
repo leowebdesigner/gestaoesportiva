c949b70b7fa09cdf1a2a82c9d26c6365c31a2434
Leonardo Pereira <contato@leowebdesigner.com.br>
2026-02-04 19:01:04 -0300

    ⚡️ feat: created resources for controllers return

---
+++ app/Http/Resources/TeamResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TeamResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'external_id' => $this->external_id,
+            'name' => $this->name,
+            'city' => $this->city,
+            'abbreviation' => $this->abbreviation,
+            'conference' => $this->conference,
+            'division' => $this->division,
+            'full_name' => $this->full_name,
+            'created_at' => $this->created_at?->toDateTimeString(),
+            'updated_at' => $this->updated_at?->toDateTimeString(),
+        ];
+    }
+}
