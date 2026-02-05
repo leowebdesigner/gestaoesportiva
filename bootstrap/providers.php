332625883d3c96927ab2348a509f02cb0d3d1cc9
Leonardo Pereira <contato@leowebdesigner.com.br>
2026-02-05 20:25:47 -0300

    feat: install horizon and config queue

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,7 +2,8 @@
 
 return [
     App\Providers\AppServiceProvider::class,
-    App\Providers\RepositoryServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
     App\Providers\ExternalServiceProvider::class,
+    App\Providers\HorizonServiceProvider::class,
+    App\Providers\RepositoryServiceProvider::class,
 ];
